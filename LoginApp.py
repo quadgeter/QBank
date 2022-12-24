772b3c396384cee8fa85040ed5c51b477e7e52cc
quadgeter <116421941+quadgeter@users.noreply.github.com>
2022-12-24 17:56:43 -0500

    Update LoginApp.py

--- LoginApp.py
+++ LoginApp.py
@@ -131,49 +131,6 @@ class ThirdWin(tk.Toplevel, App, BankAccount):
         self.balLabel = Label(self.thirdWin, text="Your current balance is: $" + str(self.bank.getBalance()) + ".")
         self.balLabel.pack()
         self.btnFrame = ThirdWin.gridFrame(self)
-                    
-    def logout(self):
-        for self.widget in self.thirdWin.winfo_children():
-            self.widget.destroy() 
-        self.goodBye = Label(self.thirdWin, text="Good Bye!", font=("Calibri", 18))
-        self.goodBye.pack(pady=30)
-        self.logoutMsg = Label(self.thirdWin, text=f"Succesfully Logged Out. \n Thank you for using QBank", font=("Calibri", 15))
-        self.logoutMsg.pack(pady=15)
-        
-
-    def depositFrame(self):
-        for widget in self.thirdWin.winfo_children():
-            widget.forget()
-        
-        self.depLabel = Label(self.thirdWin, text="Enter Amount to Deposit:", font=("Calibri", 18))
-        self.depLabel.place(x=65, y=100)
-        self.depEntry = ttk.Entry(self.thirdWin, width=15)
-        self.depEntry.place(x=140, y=150)
-        self.depBtn = ttk.Button(self.thirdWin, text="Deposit", command=partial(ThirdWin.subDeposit, self))
-        self.depBtn.place(x=150, y=200)
-        
-    def subDeposit(self):
-        print(f'Deposited ${float(self.depEntry.get())}')
-        self.bank.balance += float(self.depEntry.get())
-        print(f'Balance ${self.bank.getBalance()}')
-        self.depEntry.delete(0, END)
-        
-    def goHome(self):
-        self.depLabel.destroy()
-        self.depEntry.destroy()
-        self.depBtn.destroy()
-        
-        self.label = Label(self.thirdWin, text="Welcome " + str(self.userNameEntry), font=("Calibri", 18))
-        self.label.pack(pady=32)
-        style = ttk.Style()
-        style.configure("TButton", font=("Calibri", 9))
-        self.logoutBtn = ttk.Button(self.thirdWin, text="Logout", command=partial(ThirdWin.logout, self)) 
-        self.logoutBtn.place(x=290, y=5)
-        self.homeBtn = ttk.Button(self.thirdWin, text="Home", command=partial(ThirdWin.goHome, self))
-        self.homeBtn.place(x=10, y= 5)
-        self.balLabel = Label(self.thirdWin, text="Your current balance is: $" + str(self.bank.getBalance()) + ".")
-        self.balLabel.pack()
-        self.btnFrame = ThirdWin.gridFrame(self)
         
     def gridFrame(self):
         self.frame = Frame(self.thirdWin)   
@@ -187,13 +144,72 @@ class ThirdWin(tk.Toplevel, App, BankAccount):
         
         self.depositBtn = ttk.Button(self.frame, text="Deposit", command=partial(ThirdWin.depositFrame, self))
         self.depositBtn.grid(column=0, row=2, padx= 7, sticky="news")
-        self.withBtn = ttk.Button(self.frame, text="Withdraw")#, height=4, width=12)
+        self.withBtn = ttk.Button(self.frame, text="Withdraw", command=partial(ThirdWin.withFrame, self))
         self.withBtn.grid(column=1, row=2, padx=7, sticky="news", rowspan=2)
         self.prevTrans = ttk.Button(self.frame, text="View Previous \n Transaction")#, height=4, width=12)
         self.prevTrans.grid(column=0, row=4, pady=14, padx=7, sticky='news')
         
         self.setBtn = ttk.Button(self.frame, text="Settings")#, height=4, width=12)
         self.setBtn.grid(column=1, row=4, pady=14, padx=7, sticky="news")
+        
+                    
+    def logout(self):
+        for self.widget in self.thirdWin.winfo_children():
+            self.widget.destroy() 
+        self.goodBye = Label(self.thirdWin, text="Good Bye!", font=("Calibri", 18))
+        self.goodBye.pack(pady=30)
+        self.logoutMsg = Label(self.thirdWin, text=f"Succesfully Logged Out. \n Thank you for using QBank", font=("Calibri", 15))
+        self.logoutMsg.pack(pady=15)
+    
+    def goHome(self):      #FIXME: pressing HOME while home creates another welcome msg
+        for widget in self.thirdWin.winfo_children():
+           widget.place_forget()
+        
+        self.label = Label(self.thirdWin, text="Welcome " + str(self.userNameEntry), font=("Calibri", 18))
+        self.label.pack(pady=32)
+        style = ttk.Style()
+        style.configure("TButton", font=("Calibri", 9))
+        self.logoutBtn = ttk.Button(self.thirdWin, text="Logout", command=partial(ThirdWin.logout, self)) 
+        self.logoutBtn.place(x=290, y=5)
+        self.homeBtn = ttk.Button(self.thirdWin, text="Home", command=partial(ThirdWin.goHome, self))
+        self.homeBtn.place(x=10, y= 5)
+        self.balLabel = Label(self.thirdWin, text="Your current balance is: $" + str(self.bank.getBalance()) + ".")
+        self.balLabel.pack()
+        self.btnFrame = ThirdWin.gridFrame(self) 
+
+    def depositFrame(self):
+        for widget in self.thirdWin.winfo_children():
+            widget.forget()
+        
+        self.depLabel = Label(self.thirdWin, text="Enter Amount to Deposit:", font=("Calibri", 18))
+        self.depLabel.place(x=65, y=100)
+        self.depEntry = ttk.Entry(self.thirdWin, width=15)
+        self.depEntry.place(x=140, y=150)
+        self.depBtn = ttk.Button(self.thirdWin, text="Enter", command=partial(ThirdWin.subDeposit, self))
+        self.depBtn.place(x=150, y=200)
+        
+    def subDeposit(self):
+        self.bank.deposit(float(self.depEntry.get()))
+        print(self.bank.getPreviousTransaction())
+        print(f'Balance ${self.bank.getBalance()}')
+        self.depEntry.delete(0, END)
+        
+    def withFrame(self):
+        for widget in self.thirdWin.winfo_children():
+            widget.forget()
+        
+        self.withLabel = Label(self.thirdWin, text="Enter Amount to Withdraw:", font=("Calibri", 18))
+        self.withLabel.place(x=65, y=100)
+        self.withEntry = ttk.Entry(self.thirdWin, width=15)
+        self.withEntry.place(x=140, y=150)
+        self.withBtn = ttk.Button(self.thirdWin, text="Enter", command=partial(ThirdWin.subWith, self))
+        self.withBtn.place(x=150, y=200)
+        
+    def subWith(self):
+        self.bank.withdraw(float(self.withEntry.get()))
+        print(self.bank.getPreviousTransaction())
+        print(f'Balance ${self.bank.getBalance()}')
+        self.withEntry.delete(0, END)
         
    
 def main():
